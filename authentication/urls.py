b6180f7a257414fca769d0bab9d459f15bb1f86c
hsbakir <94084650+DEVBakir@users.noreply.github.com>
2024-06-01 13:56:47 +0100

    Update Refresh Token Name

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,5 +7,5 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
